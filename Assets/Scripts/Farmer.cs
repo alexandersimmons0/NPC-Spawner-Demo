2c705ab0cd4e5a5e6c3cb28764203cbe78fea94f
alexandersimmons0 <simmoa14@my.erau.edu>
2023-10-15 20:47:04 -0700

    Initial commit

---
+++ Assets/Scripts/Farmer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farmer : INPC{
+    public void Speak(){
+        Debug.Log("You reap what you sow");
+    }
+}
---
+++ Assets/Scripts/Shopowner.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shopowner : INPC{
+    public void Speak(){
+        Debug.Log("Do you wish to purchase something");
+    }
+}
